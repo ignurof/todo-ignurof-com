6bf4069973ca9fd6d02b8fa2c1e2d2dfa43002f3
ignurof <ignurof@ignurof.com>
2024-05-08 14:11:53 +0200

    Add task model

--- src/todo/models.py
+++ src/todo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    def __init__(self):
+        print('init Task model')
+
+    def __str__(self):
+        return self.title
+
+    title = models.CharField(max_length=20)
+    text = models.CharField(max_length=120)
