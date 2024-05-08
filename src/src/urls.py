f000214883520f4b0d0ea29baeb762a842b96dca
ignurof <ignurof@ignurof.com>
2024-05-08 15:47:37 +0200

    Fix urlpatterns wrong type

--- src/src/urls.py
+++ src/src/urls.py
@@ -17,8 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = { 
+urlpatterns = [
     path('', include('todo.urls')),
 
     path('admin/', admin.site.urls),
-}
+]
--- src/todo/urls.py
+++ src/todo/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 app_name = 'todo'
 
-urlpatterns = {
+urlpatterns = [ 
     path('', views.index, name='index')
-}
+]
